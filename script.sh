ab70fb120b0e0dbdfb711d5dd6b0e1d989f48447
root <root@puppet.local>
2022-03-08 21:00:14 +0200

    Pushing

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Start of the script -----------------"
+
+echo "Adding the numbers"
+
+sum=$(($num1+$num2))
+
+echo "The sum is $sum"
+
+echo "Script finished --------------------"
+
+
